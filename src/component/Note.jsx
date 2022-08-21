bfe7481f599ab6ba98df56f3da1cccd2d4bab551
Edgar <edgarx28@outlook.com>
2022-08-20 22:27:01 -0400

    Initial commit

---
+++ src/component/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Note() {
+    return (
+        <div className="note">
+            <h1 className="note-h1">Test title</h1>
+            <p className="note-p">Test content</p>
+        </div>
+    );
+}
+
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./component/App";
+
+
+
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Initial render
+root.render(
+
+    <App />
+
+
+);
+
+
+
+
+
+
+
+
